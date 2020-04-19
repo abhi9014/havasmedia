009e0db7029f2391721a6392394c094f6d9dde18
John Doeabhishek-genisysgit config --global user.name abhishek-genisysAbhishek.C@GISI-MP18PWSM MINGW64 /e/GIT/Test$ git config --global user.name John <Abhishek.C@GISI-MP18PWSM.genblr.local>
2020-04-19 18:50:06 +0530

    deleted files

--- src/main/java/com/havasmedia/basetest/MainRunner.java
+++ src/main/java/com/havasmedia/basetest/MainRunner.java
@@ -3,29 +3,11 @@ package com.havasmedia.basetest;
 
 
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.Scenario;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-@CucumberOptions(
-				features ="classpath:FeaturesFiles",
-				glue= {"com.havasmedia.campaigntest","com.havasmedia.testStepDefination"},
-				plugin = {"pretty","json:target/CucumberTestReport.json",
-							"junit:target/surefire-reports/TESTCucumber.xml"},
-				monochrome = true, //display the console output in a proper readable format
-				strict = true, //it will check if any step is not defined in step definition file
-				dryRun = false,//to check the mapping is proper between feature file and step def file
-				tags= {"@BDDTEST-TES-4"}//"~@Ignore"   "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/cucumber-reports/report.html
-		)
+public class MainRunner{
+	
+	public static void main(String[] args) {
+		
+	}
 
-public class MainRunner extends AbstractTestNGCucumberTests{
-	public static Scenario scenario;
-	/*
-	 * @AfterClass public static void writeExtentReport() { String path
-	 * =BaseConfigurationSetup.USER_DIRECTORY+
-	 * "\\src\\main\\resources\\ConfigProperties\\extentReport.xml";
-	 * System.out.println(path); Reporter.loadXMLConfig(path); //
-	 * Reporter.loadXMLConfig(".
-	 *src/main/resources/ConfigProperties/extent-config");Reporter.setSystemInfo("User Name",System.getProperty("user.name"));Reporter.setSystemInfo("Time Zone",System.getProperty("user.timezone"));Reporter.setSystemInfo("Machine","Windows 10"+"64 Bit");Reporter.setSystemInfo("Selenium","3.7.0");Reporter.setSystemInfo("Maven","3.5.2");Reporter.setSystemInfo("Java Version","1.8.0_151");
-}*/
 }
 	
--- src/test/java/com/demo/testngTest/ReadTestdata.java
+++ src/test/java/com/demo/testngTest/ReadTestdata.java
@@ -1,5 +1,13 @@
 package com.demo.testngTest;
 
-public class ReadTestdata {
+import org.testng.annotations.Test;
 
+import com.havasmedia.basetest.BaseConfigurationSetup;
+
+public class ReadTestdata extends BaseConfigurationSetup {
+
+	@Test
+	public void test() {
+		System.out.println();
+	}
 }
